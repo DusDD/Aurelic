1a70857411483fca07fd5f365c8242b638750d29
nathanbecker <nl.becker@yahoo.com>
2026-01-06 21:42:15 +0100

    Erste Implementierungen der Startpage.

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-from gui.user_interface import main_window
+# main.py
+#!/usr/bin/env python3
+import sys
+from PySide6.QtWidgets import QApplication
+from gui.startpage import StartPage
 
-if __name__ == '__main__':
-    main_window()
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    w = StartPage()
+    w.showMaximized()  # fills the screen
+
+    sys.exit(app.exec())
